826b595c9db3cdd83c888190aaac3c5a07444e0b
Rubén Alcalde Sebastián <ruben.alcalde@ce.eurecat.org>
2022-08-03 00:12:19 +0200

    Products categories seeders

---
+++ database/seeders/HygieneCategorySeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class HygieneCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('hygiene_category')->insert([
+            ['id' => 1,
+            'name' => 'detergent']
+           ,[
+           	'id' => 2,
+           	'name' => 'gel'
+            ],[
+           	'id' => 3,
+           	'name' => 'shampoo'
+            ],[
+           	'id' => 4,
+           	'name' => 'toothpaste'
+            ],[
+           	'id' => 5,
+           	'name' => 'fluoride'
+            ],[
+           	'id' => 6,
+           	'name' => 'liquid contact sense'
+            ],[
+           	'id' => 7,
+           	'name' => 'others'
+            ]
+        ]);
+    }
+}
